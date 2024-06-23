552e2030a75f208c304e6e0181819108caf41a22
Juan Cardona <felipe7u7432@gmail.com>
2024-06-23 15:49:15 -0500

    :truck:  refactor (Assembler) assemblers are moved to core

--- Core/src/main/java/co/edu/uco/core/application/facade/message/implementation/ListMessageFacadeImpl.java
+++ Core/src/main/java/co/edu/uco/core/application/facade/message/implementation/ListMessageFacadeImpl.java
@@ -2,26 +2,26 @@ package co.edu.uco.core.application.facade.message.implementation;
 
 import co.edu.uco.core.application.dto.MessageDTO;
 import co.edu.uco.core.application.facade.message.ListMessageFacade;
+import co.edu.uco.core.assembler.dto.DTOAssembler;
 import co.edu.uco.core.domain.domains.MessageDomain;
 import co.edu.uco.core.domain.port.in.ListMessageInPort;
-import co.edu.uco.utils.mapper.assembler.ModelAssembler;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ListMessageFacadeImpl implements ListMessageFacade {
 
-    private final ModelAssembler assembler;
+    private final DTOAssembler<MessageDTO, MessageDomain> assembler;
     private final ListMessageInPort useCase;
 
-    public ListMessageFacadeImpl(ModelAssembler assembler, ListMessageInPort useCase) {
+    public ListMessageFacadeImpl(DTOAssembler<MessageDTO, MessageDomain> assembler, ListMessageInPort useCase) {
         this.assembler = assembler;
         this.useCase = useCase;
     }
 
     @Override
     public void execute(MessageDTO dto, HttpServletResponse response) {
-        MessageDomain messageDomain = assembler.assembleDomain(dto, MessageDomain.class);
+        MessageDomain messageDomain = assembler.assembleDomain(dto);
         useCase.execute(messageDomain, response);
     }
 }
