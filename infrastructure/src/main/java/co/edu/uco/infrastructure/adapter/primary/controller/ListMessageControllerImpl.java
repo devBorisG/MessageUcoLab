622f0469b6803d96953bae7d497f5a2cb42665c1
David Andrés Valencia García <david.andres.2801@gmail.com>
2024-06-23 17:44:02 -0500

    :recycle: REFACTOR: Has been added the ListMessageController interface to implementation

--- infrastructure/src/main/java/co/edu/uco/infrastructure/adapter/primary/controller/ListMessageControllerImpl.java
+++ infrastructure/src/main/java/co/edu/uco/infrastructure/adapter/primary/controller/ListMessageControllerImpl.java
@@ -3,17 +3,18 @@ package co.edu.uco.infrastructure.adapter.primary.controller;
 import co.edu.uco.core.application.dto.MessageDTO;
 import co.edu.uco.core.domain.usecase.handling.HandlingListMessageInputPort;
 import co.edu.uco.infrastructure.adapter.AbstractRestController;
+import co.edu.uco.infrastructure.adapter.primary.ListMessageController;
 import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/messageucolab/v1/message")
-public class ListMessageController extends AbstractRestController {
+public class ListMessageControllerImpl extends AbstractRestController implements ListMessageController {
 
     private final HandlingListMessageInputPort handlingListMessageInputPort;
 
-    public ListMessageController(HandlingListMessageInputPort handlingListMessageInputPort) {
+    public ListMessageControllerImpl(HandlingListMessageInputPort handlingListMessageInputPort) {
         this.handlingListMessageInputPort = handlingListMessageInputPort;
     }
 
