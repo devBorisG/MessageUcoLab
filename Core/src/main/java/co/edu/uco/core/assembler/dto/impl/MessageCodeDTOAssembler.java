1673ebffa46ebae26c17c70a1552f4e51d59efcc
Juan Cardona <felipe7u7432@gmail.com>
2024-08-03 11:54:41 -0500

    :sparkles:  FEAT: add MessageCodeDTO

---
+++ Core/src/main/java/co/edu/uco/core/assembler/dto/impl/MessageCodeDTOAssembler.java
@@ -0,0 +1,18 @@
+package co.edu.uco.core.assembler.dto.impl;
+
+import co.edu.uco.core.application.dto.MessageCodeDTO;
+import co.edu.uco.core.assembler.dto.DTOAssembler;
+import co.edu.uco.core.domain.domains.MessageDomain;
+
+public class MessageCodeDTOAssembler implements DTOAssembler<MessageCodeDTO, MessageDomain> {
+
+    @Override
+    public MessageDomain assembleDomain(MessageCodeDTO dto) {
+        return null;
+    }
+
+    @Override
+    public MessageCodeDTO assembleDTO(MessageDomain domain) {
+        return null;
+    }
+}
