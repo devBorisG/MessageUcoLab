7869e05e7901a85916b4704b02b91376c264c2c8
David Andrés Valencia García <david.andres.2801@gmail.com>
2024-08-10 20:01:16 -0500

    :sparkles: NEW FEATURE: Creation of basic exceptions for the project

--- Utils/src/main/java/co/edu/uco/utils/exception/CrossWordsException.java
+++ Utils/src/main/java/co/edu/uco/utils/exception/CrossWordsException.java
@@ -1,23 +1,26 @@
 package co.edu.uco.utils.exception;
 
+import java.io.Serial;
+
 import static co.edu.uco.utils.helper.UtilObject.getUtilObject;
 import static co.edu.uco.utils.helper.UtilText.getUtilText;
 
-public class GeneralException extends RuntimeException {
+public class CrossWordsException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = -3393801537376736983L;
     private String userMessage;
     private String technicalMessage;
     private Exception rootException;
 
-    public static GeneralException build(String technicalMessage) {
-        return new GeneralException(null, technicalMessage, null);
+    public static CrossWordsException build(String technicalMessage) {
+        return new CrossWordsException(null, technicalMessage, null);
     }
 
-    public static GeneralException build(String technicalMessage, Exception rootException) {
-        return new GeneralException(null, technicalMessage, rootException);
+    public static CrossWordsException build(String technicalMessage, Exception rootException) {
+        return new CrossWordsException(null, technicalMessage, rootException);
     }
 
-    protected GeneralException(String userMessage, String technicalMessage, Exception rootException) {
+    protected CrossWordsException(String userMessage, String technicalMessage, Exception rootException) {
         super();
         setUserMessage(userMessage);
         setTechnicalMessage(technicalMessage);
@@ -39,7 +42,7 @@ public class GeneralException extends RuntimeException {
     private void setTechnicalMessage(String technicalMessage) {
         this.technicalMessage = getUtilText().trim(technicalMessage);
     }
-    private void setRootException(Exception rootException) {
+    private void setRootException(final Exception rootException) {
         this.rootException = getUtilObject().getDefaultIsNull(rootException, new Exception());
     }
 
