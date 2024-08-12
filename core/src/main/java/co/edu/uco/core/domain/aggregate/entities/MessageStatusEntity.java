3e99cd1cd7d1ad85d43218965209ee0cb96a0051
Juan Cardona <felipe7u7432@gmail.com>
2024-08-12 16:04:31 -0500

    :recycle: : REFACTOR: Extends entities of the abstract class Entity

--- core/src/main/java/co/edu/uco/core/domain/aggregate/entities/MessageStatusEntity.java
+++ core/src/main/java/co/edu/uco/core/domain/aggregate/entities/MessageStatusEntity.java
@@ -1,13 +1,12 @@
 package co.edu.uco.core.domain.aggregate.entities;
 
+import co.edu.uco.core.domain.aggregate.Entity;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.UUID;
 
 @Getter
-@Setter
-public class MessageStatusEntity {
+public class MessageStatusEntity extends Entity<UUID> {
     private UUID id;
     private String name;
 }
