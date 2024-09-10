a2726b66ea3af0dcb612cd6e394dffc508b852a2
federico.zapata <fede.zapata6@gmail.com>
2024-09-09 21:56:31 -0500

    :recycle: REFACTOR Refactorizar el código de consumo de mensajes en caché o en mongodb

--- Core/src/main/java/co/edu/uco/core/domain/usecase/ListMessageCacheUseCase.java
+++ Core/src/main/java/co/edu/uco/core/domain/usecase/ListMessageCacheUseCase.java
@@ -5,7 +5,6 @@ import co.edu.uco.core.assembler.pojo.Message;
 import co.edu.uco.core.domain.domains.MessageCodeDomain;
 import co.edu.uco.core.domain.port.in.ListMessageCacheInPort;
 import co.edu.uco.core.domain.port.out.db.mongo.IMongoRepository;
-import co.edu.uco.core.domain.port.out.db.redis.RedisMessage;
 import co.edu.uco.core.domain.port.out.presenter.ListMessagePresenter;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -21,38 +20,30 @@ public class ListMessageCacheUseCase implements ListMessageCacheInPort {
 
     private final ListMessagePresenter presenter;
     private final IMongoRepository mongoRepository;
-    private final RedisMessage redisRepository;
     private final RedisTemplate<String, Message> redisTemplate;
+    private String message;
 
     @Autowired
-    public ListMessageCacheUseCase(ListMessagePresenter presenter, IMongoRepository mongoRepository, RedisMessage redisRepository, RedisTemplate<String, Message> redisTemplate) {
+    public ListMessageCacheUseCase(ListMessagePresenter presenter, IMongoRepository mongoRepository, RedisTemplate<String, Message> redisTemplate) {
         this.presenter = presenter;
         this.mongoRepository = mongoRepository;
-        this.redisRepository = redisRepository;
         this.redisTemplate = redisTemplate;
     }
 
     @Override
     public void execute(MessageCodeDomain entity, HttpServletResponse response) {
-        Message message = new Message();
-        message.setCode("1");
-        message.setContent("Hello World");
-        message.setApplication("AAplication");
-        message.setStatus("Active");
-        message.setFunctionality("Functionality");
-        message.setType("Type");
-        message.setTitle("Title");
-        message.setId("098");
         try {
-            //log.info(Objects.requireNonNull(redisTemplate.opsForValue().get("message:125")).getContent());
             if (redisTemplate.opsForValue().get(entity.getCode()) != null) {
                 log.info((Objects.requireNonNull(redisTemplate.opsForValue().get(entity.getCode())).getContent()));
+                message = "Por redis " + Objects.requireNonNull(redisTemplate.opsForValue().get(entity.getCode())).getContent();
             } else {
                 mongoRepository.findAllSelf(entity.getCode()).forEach(message1 -> {
                     redisTemplate.opsForValue().set("message:126", message1);
                     log.info(message1.getContent());
+                    message = "Por mongo " + message1.getContent();
                 });
             }
+            presenter.execute(MessageCodeDTO.create(message), response);
         } catch (Exception exception) {
             System.out.println("Error: " + exception.getMessage());
         }
