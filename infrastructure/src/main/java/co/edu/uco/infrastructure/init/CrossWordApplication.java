3d8c90afefed58597fc4262bd600621721713a62
Juan Felipe Cardona <juan.cardona1338@uco.net.co>
2024-10-24 09:39:40 -0600

    :bug: FIX:  Add property for scanning redis adapter
    Issue: #38

--- infrastructure/src/main/java/co/edu/uco/infrastructure/init/CrossWordApplication.java
+++ infrastructure/src/main/java/co/edu/uco/infrastructure/init/CrossWordApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @ComponentScan(basePackages = {"co.edu.uco"})
 @EnableMongoRepositories(basePackages = {"co.edu.uco.infrastructure.adapter.secondary.repository"})
+@EnableRedisRepositories(basePackages = {"co.edu.uco.infrastructure.adapter.secondary.repository"})
 @EnableCaching
 public class CrossWordApplication {
     public static void main(String[] args) {
