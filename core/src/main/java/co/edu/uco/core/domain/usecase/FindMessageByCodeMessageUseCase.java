f97155a5af5e61342f2e7c13f4fba4c8f7c1a4a8
David Andrés Valencia García <david.andres.2801@gmail.com>
2024-11-14 12:47:26 -0500

    :recycle: REFACTOR: Remove unused imports.
    Issue: #43

--- core/src/main/java/co/edu/uco/core/domain/usecase/FindMessageByCodeMessageUseCase.java
+++ core/src/main/java/co/edu/uco/core/domain/usecase/FindMessageByCodeMessageUseCase.java
@@ -12,9 +12,7 @@ import co.edu.uco.utils.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 @Service
 @Slf4j
