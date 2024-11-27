3662ea2060d5cd5f91054f80b49f37623461a368
Juan Felipe Cardona <juan.cardona1338@uco.net.co>
2024-11-27 15:10:06 -0600

    :recycle: REFACTOR: Change MessageDTO to Record
    Issue: #46, #UCO-11

--- core/src/main/java/co/edu/uco/core/application/dto/MessageDTO.java
+++ core/src/main/java/co/edu/uco/core/application/dto/MessageDTO.java
@@ -1,45 +1,20 @@
 package co.edu.uco.core.application.dto;
 
-import lombok.Getter;
-
-import java.util.UUID;
-
 import static co.edu.uco.utils.helper.UtilText.trim;
-import static co.edu.uco.utils.helper.UtilUUID.getDefaultUUID;
 
-@Getter
-public class MessageDTO {
-
-    private UUID id;
-    private String code;
-    private String title;
-    private String content;
-    private String application;
-    public MessageDTO() {
-    }
-    public MessageDTO(UUID id, String code, String title, String content, String application) {
-        setId(id);
-        setCode(code);
-        setTitle(title);
-        setContent(content);
-        setApplication(application);
-    }
-    public void setId(UUID id) {
-        this.id = getDefaultUUID(id);
-    }
-    public void setCode(String code) {
+public record MessageDTO(String code, String title, String content, String type, String category, String application,
+                         String functionality) {
+    public MessageDTO(String code, String title, String content, String type, String category, String application, String functionality) {
         this.code = trim(code);
-    }
-    public void setTitle(String title) {
         this.title = trim(title);
-    }
-    public void setContent(String content) {
         this.content = trim(content);
-    }
-    public void setApplication(String application) {
         this.application = trim(application);
+        this.type = trim(type);
+        this.category = trim(category);
+        this.functionality = trim(functionality);
     }
-    public static MessageDTO create(UUID id, String code, String title, String content, String application) {
-        return new MessageDTO(id, code, title, content, application);
+
+    public static MessageDTO create(String code, String title, String content, String type, String category, String application, String functionality) {
+        return new MessageDTO(code, title, content, type, category, application, functionality);
     }
 }
