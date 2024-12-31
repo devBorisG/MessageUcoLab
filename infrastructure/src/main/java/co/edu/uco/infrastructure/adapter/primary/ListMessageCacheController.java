823bc30c8802790a2c293d40e7a74a2d016ed8ff
F3liP3L <felipe7u7432@gmail.com>
2024-12-31 13:55:45 -0500

    :fire: REMOVE: Remove dead and unnecessary code
    Issue: #55, #UCO-17

--- infrastructure/src/main/java/co/edu/uco/infrastructure/adapter/primary/ListMessageCacheController.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uco.infrastructure.adapter.primary;
-
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface ListMessageCacheController {
-    void execute(String codeMessage, HttpServletResponse response);
-}
