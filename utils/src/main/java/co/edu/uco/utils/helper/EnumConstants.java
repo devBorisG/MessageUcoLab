5fda06739c3bb2c491b0c2fad721cf433ff30cbe
F3liP3L <felipe7u7432@gmail.com>
2024-12-31 13:32:15 -0500

    :recycle: REFACTOR: Add constant in UtilMapperJson
    Issue: #55, #UCO-17

--- utils/src/main/java/co/edu/uco/utils/helper/EnumConstants.java
+++ utils/src/main/java/co/edu/uco/utils/helper/EnumConstants.java
@@ -6,13 +6,10 @@ public enum EnumConstants {
     SIMPLE_PAGE_QUERY("%"),
     DATE_FORMAT("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"),
     DEFAULT_UUID_STRING("00000000-0000-0000-0000-000000000000");
-
     private final String value;
-
     EnumConstants(String value) {
         this.value = value;
     }
-
     public String getValue() {
         return value;
     }
