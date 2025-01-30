7e1352474aecf9aa9a446ab462acaa8881e1451a
F3liP3L <felipe7u7432@gmail.com>
2025-01-30 10:44:48 -0500

    :recycle: REFACTOR: Add constants in LogginConfig
    Issue: #61

--- core/src/main/java/co/edu/uco/core/CrosswordsConstant.java
+++ core/src/main/java/co/edu/uco/core/CrosswordsConstant.java
@@ -13,5 +13,11 @@ public final class CrosswordsConstant {
     public static final String FIELD_MONGO_ADAPTER_STATUS = "_airbyte_data.status";
     public static final String FIELD_MONGO_ADAPTER_APPLICATION = "_airbyte_data.application";
     public static final String FIELD_MONGO_ADAPTER_FUNCTIONALITY = "_airbyte_data.functionality";
+    public static final String LOGGING_REQUEST_URI = "REQUEST_URI";
+    public static final String LOGGING_HTTP_METHOD = "HTTP_METHOD";
+    public static final String LOGGING_SESSION_ID = "SESSION_ID";
+    public static final String LOGGING_QUERY_STRING = "QUERY_STRING";
+    public static final String LOGGING_PARAMETER_CODE_MESSAGE = "codeMessage";
+    public static final String LOGGING_PARAMETER_APPLICATION = "application";
     public static final String REQUEST_COLUMN_SORT_DEFAULT = "id";
 }
--- infrastructure/src/main/java/co/edu/uco/infrastructure/configuration/LoggingConfig.java
+++ infrastructure/src/main/java/co/edu/uco/infrastructure/configuration/LoggingConfig.java
@@ -6,21 +6,20 @@ import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import static co.edu.uco.core.CrosswordsConstant.*;
+
 @Component
-public class LoggingConfig implements HandlerInterceptor {
-
+public final class LoggingConfig implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request,HttpServletResponse response , Object handler ){
-        MDC.put("REQUEST_URI", request.getRequestURI());
-        MDC.put("HTTP_METHOD", request.getMethod());
-        MDC.put("SESSION_ID", request.getSession().getId());
-        MDC.put("QUERY_STRING", request.getQueryString());
-        MDC.put("codeMessage",request.getParameter("codeMessage"));
-        MDC.put("application",request.getParameter("application"));
-
+    public boolean preHandle(HttpServletRequest request,HttpServletResponse response,Object handler ){
+        MDC.put(LOGGING_REQUEST_URI, request.getRequestURI());
+        MDC.put(LOGGING_HTTP_METHOD, request.getMethod());
+        MDC.put(LOGGING_SESSION_ID, request.getSession().getId());
+        MDC.put(LOGGING_QUERY_STRING, request.getQueryString());
+        MDC.put(LOGGING_PARAMETER_CODE_MESSAGE,request.getParameter(LOGGING_PARAMETER_CODE_MESSAGE));
+        MDC.put(LOGGING_PARAMETER_APPLICATION,request.getParameter(LOGGING_PARAMETER_APPLICATION));
         return true;
     }
-
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response , Object handler , Exception exception){
         MDC.clear();
