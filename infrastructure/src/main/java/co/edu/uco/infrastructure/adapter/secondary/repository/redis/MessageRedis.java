f4f3c2185d0ed5e4af41a3486421822c7addcaee
F3liP3L <felipe7u7432@gmail.com>
2025-01-30 19:01:31 -0500

    :recycle: REFACTOR: Add constants
    Issue: #64

--- infrastructure/src/main/java/co/edu/uco/infrastructure/adapter/secondary/repository/redis/MessageRedis.java
+++ infrastructure/src/main/java/co/edu/uco/infrastructure/adapter/secondary/repository/redis/MessageRedis.java
@@ -2,6 +2,7 @@ package co.edu.uco.infrastructure.adapter.secondary.repository.redis;
 
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
